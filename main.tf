b0c3bccbcc2edeea6fca1c8915ed04ef099b73b4
Nishant Singh <nishantsingh.bhilai@gmail.com>
2026-04-06 20:48:49 +0530

    Add AWS infrastructure resources for VPC and instance

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "banking_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.banking_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+}
